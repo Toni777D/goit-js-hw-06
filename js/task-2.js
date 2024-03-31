33f9e082eacd2e879c62e15f00b86cf3e89e57fd
Toni777D <39744158+Toni777D@users.noreply.github.com>
2024-03-31 18:03:09 +0200

    hw-06.2

--- js/task-2.js
+++ js/task-2.js
@@ -13,7 +13,7 @@ class Storage {
     this.#items.push(newItem);
   }
   removeItem(itemToRemove) {
-    this.#items.filter(item => item !== itemToRemove);
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 

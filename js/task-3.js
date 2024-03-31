7b6e79025e5fd978e530701c9f982d113ad6e481
Toni777D <39744158+Toni777D@users.noreply.github.com>
2024-03-31 12:05:09 +0200

    hw-06.1

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,32 @@
-function getElementWidth(content, padding, border) {
-  const box = Number.parseInt(content);
-  const pad = Number.parseInt(padding);
-  const bor = Number.parseFloat(border);
-  return box + pad * 2 + bor * 2;
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
 
-getElementWidth();
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
